c43c4b0f35653a01bbfb8da6a9e22a88a52463ad
hexmo <hitmo3205@gmail.com>
2020-02-06 13:24:52 +0545

    Created static files to locally store bootstrap and jquery files

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -30,7 +30,7 @@ urlpatterns += [
     path('',include('productmanagement.urls')),
     path('',include('search.urls')),
     path('',include('userManagement.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
