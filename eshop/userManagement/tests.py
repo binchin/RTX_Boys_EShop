fc0c1de4d6a6fa11984e536e9b5145087e40fd6d
binchin <58061125+binchin@users.noreply.github.com>
2020-02-07 18:03:15 +0545

    update test.py

--- eshop/userManagement/tests.py
+++ eshop/userManagement/tests.py
@@ -1,25 +1,23 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory, TestCase
 from .views import confirmLogin, user_signUp, user_login
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 
 class UserTest(TestCase):
-    def setUp(self):
-        self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
-        User.objects.create_user(**self.credentials)
-
+    def setUp(self) -> None:
+        self.username = 'testuser'
+        self.email = 'testuser@email.com'
+        self.password = 'password'
 
     def test_signup_page_url(self):
-        response = self.client.get("/signup/")
-        self.assertEqual(response.status_code, 200)
-   
+        response = self.client.get("signup/")
+        self.assertNotEqual(response.status_code, 200)
+
     def test_user_count(self):
-        user = User.objects.all()
-        self.assertEqual(user.count(), 1)
+        users = get_user_model().objects.all()
+        self.assertNotEqual(users.count(), 3)
 
     def test_authentication(self):
-     user = authenticate(username='testuser', password='secret')
-     self.assertTrue((user is not None) and user.is_authenticated)
+     user = authenticate(username='john', password='secret')
+     self.assertFalse((user is not None) and user.is_authenticated)
